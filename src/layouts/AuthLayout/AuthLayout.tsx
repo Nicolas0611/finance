ea7a05e3346f966c4ef3063dd04ff887b7734796
Nicolás Sierra <nico.sierra13@gmail.com>
2026-05-17 18:11:39 -0500

    Remove AuthLayout component and its associated styles to streamline the authentication structure.

--- src/layouts/AuthLayout/AuthLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-const AuthLayout = () => (
-  <div className="h-screen bg-canvas">
-    <Outlet />
-  </div>
-);
-
-export default AuthLayout;
--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,2 +1,1 @@
-export { default as AuthLayout } from './AuthLayout/AuthLayout'
-export { default as MainLayout } from './MainLayout/MainLayout'
+export { default as MainLayout } from "./MainLayout/MainLayout";
