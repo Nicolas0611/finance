17f60212f738370a109ad2ad3e66984eb15dd6c4
Nicolás Sierra <nico.sierra13@gmail.com>
2026-05-17 18:10:02 -0500

    Refactor public routes to include AuthLayout, LoginPage, and SignupPage; update routing structure for improved authentication flow.

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -2,17 +2,38 @@
 import { lazy } from "react";
 import { Navigate, type RouteObject } from "react-router-dom";
 
-const LoginPage = lazy(() => import("@/pages/LoginPage/LoginPage"));
+const LoginPage = lazy(() => import("@/pages/public/LoginPage/LoginPage"));
+const SignupPage = lazy(() => import("@/pages/public/SignupPage/SignupPage"));
+const AuthLayout = lazy(() => import("@/features/auth/layouts/AuthLayout"));
 
 const PUBLIC_ROUTE = {
   ROOT: "/",
-  LOGIN: "/login",
+  AUTH: "/auth",
+  LOGIN: "login",
+  SIGNUP: "signup",
 };
 
 export const publicRoutes: RouteObject[] = [
   {
     path: "*",
-    element: <Navigate to={PUBLIC_ROUTE.LOGIN} />,
+    element: <Navigate to="/auth/login" replace />,
   },
-  { path: PUBLIC_ROUTE.LOGIN, element: <LoginPage /> },
+  {
+    path: PUBLIC_ROUTE.AUTH,
+    element: <AuthLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="login" replace />,
+      },
+      {
+        path: PUBLIC_ROUTE.LOGIN,
+        element: <LoginPage />,
+      },
+      {
+        path: PUBLIC_ROUTE.SIGNUP,
+        element: <SignupPage />,
+      },
+    ],
+  },
 ];
