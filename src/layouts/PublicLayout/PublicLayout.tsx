23d0c45607caca8c15a170b9ca0dfdf2f58dd01e
Nicolás Sierra <nico.sierra13@gmail.com>
2026-05-17 22:49:33 -0500

    Remove unused authentication logic from PublicLayout component to simplify structure and improve readability.

--- src/layouts/PublicLayout/PublicLayout.tsx
+++ src/layouts/PublicLayout/PublicLayout.tsx
@@ -1,17 +1,8 @@
 import { AuthLayout } from "@/features/auth/layouts";
-import { useAuth } from "@/providers/AuthProvider";
-import { useEffect } from "react";
+
 import { Outlet } from "react-router-dom";
 
 const PublicLayout = () => {
-  const { isAuthenticated, logout } = useAuth();
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      logout();
-    }
-  }, [isAuthenticated]);
-
   return (
     <AuthLayout>
       <Outlet />
