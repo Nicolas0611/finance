c25d5310ed440961c60c9e47cd7282ec8a7d925a
Nicolás Sierra <nico.sierra13@gmail.com>
2026-05-17 21:29:59 -0500

    Refactor useLogin hook to utilize AuthProvider for login and logout functionality; navigate to private route on successful login and handle errors appropriately.

--- src/features/auth/hooks/useAuthMutations.ts
+++ src/features/auth/hooks/useAuthMutations.ts
@@ -1,17 +1,21 @@
 import { useMutation } from "@tanstack/react-query";
 import { login } from "../services/authService";
-import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { useAuth } from "@/providers/AuthProvider";
+import { useNavigate } from "react-router-dom";
+import { PRIVATE_ROUTE } from "@/routes/PrivateRoutes";
 
 export const useLogin = () => {
-  const [, setToken] = useLocalStorage<string | null>("access_token", null);
-
+  const { login: loginFn, logout: logoutFn } = useAuth();
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: login,
     onSuccess: (data) => {
-      setToken(data.token);
+      loginFn(data.token);
+      navigate(PRIVATE_ROUTE.OVERVIEW);
     },
     onError: (error) => {
       console.error(error);
+      logoutFn();
     },
   });
 };
