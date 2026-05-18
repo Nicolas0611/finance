7d7d4768621a35c3f757ee72dea72802c1e527df
Nicolás Sierra <nico.sierra13@gmail.com>
2026-05-17 22:09:09 -0500

    Add SignupForm component with form handling and validation using react-hook-form; update SignupPage to render SignupForm.

--- src/features/auth/components/SignupForm/SignupForm.tsx
+++ src/features/auth/components/SignupForm/SignupForm.tsx
@@ -1,5 +1,53 @@
+import { useForm } from "react-hook-form";
+import AuthWrapper from "../AuthWrapper/AuthWrapper";
+import { Button, InputField } from "@/components";
+
+interface SignupFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignupForm = () => {
-  return <div>SignupForm</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<SignupFormData>({});
+
+  const onSubmit = (data: SignupFormData) => {
+    console.log(data);
+  };
+
+  return (
+    <AuthWrapper
+      title="Signup"
+      onSubmit={handleSubmit(onSubmit)}
+      link={{
+        title: "Already have an account?",
+        text: "Login",
+        to: "/auth/login",
+      }}
+    >
+      <InputField
+        placeholder="Enter your name"
+        {...register("name", { required: true })}
+      />
+      <InputField
+        type="email"
+        placeholder="Enter your email"
+        {...register("email", { required: true })}
+      />
+      <InputField
+        type="password"
+        placeholder="Enter your password"
+        {...register("password", { required: true })}
+      />
+      <Button type="submit" disabled={!isValid}>
+        Signup
+      </Button>
+    </AuthWrapper>
+  );
 };
 
 export default SignupForm;
--- src/pages/public/SignupPage/SignupPage.tsx
+++ src/pages/public/SignupPage/SignupPage.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import { SignupForm } from "@/features/auth/components";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return <SignupForm />;
 };
 
 export default SignupPage;
