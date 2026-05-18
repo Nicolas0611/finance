126daff594f3c45f5bdc60a7958b595ecd295e63
Nicolás Sierra <nico.sierra13@gmail.com>
2026-05-17 21:30:32 -0500

    Refactor MainLayout to accept children as props instead of using Outlet for improved flexibility in layout rendering.

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,19 +1,19 @@
-import { Outlet } from 'react-router-dom'
-import { Sidebar } from '@/components'
-import { useUiStore } from '@/store/uiStore'
-import { mainLayoutStyles as cls } from './MainLayout.styles'
+import { Sidebar } from "@/components";
+import { useUiStore } from "@/store/uiStore";
+import { mainLayoutStyles as cls } from "./MainLayout.styles";
 
- const MainLayout = () => {
-  const { sidebarOpen, toggleSidebar } = useUiStore()
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+const MainLayout = ({ children }: MainLayoutProps) => {
+  const { sidebarOpen, toggleSidebar } = useUiStore();
 
   return (
     <div className={cls.root}>
       <Sidebar isMinimized={!sidebarOpen} onToggleMinimize={toggleSidebar} />
-      <main className={cls.main}>
-        <Outlet />
-      </main>
+      <main className={cls.main}>{children}</main>
     </div>
-  )
-}
+  );
+};
 
 export default MainLayout;
