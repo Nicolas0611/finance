3e92b3f85a67f438c7a6feada61cb7a95e5d4e65
Nicolás Sierra <nico.sierra13@gmail.com>
2026-05-20 15:03:44 -0500

    Add PageDescription export to index.ts for improved layout component accessibility.

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,3 +1,4 @@
 export { default as MainLayout } from "./MainLayout/MainLayout";
 export { default as PublicLayout } from "./PublicLayout/PublicLayout";
 export { default as PrivateLayout } from "./PrivateLayout/PrivateLayout";
+export { default as PageDescription } from "./PageDescription/PageDescription";
