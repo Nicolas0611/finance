2f85b7b2b0eedb34a38598a42a0eaeb1dd63dee4
Nicolás Sierra <nico.sierra13@gmail.com>
2026-05-20 14:02:03 -0500

    Refactor component exports in index.ts for consistency and update TransactionsPage layout for improved readability.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
-export { default as Button } from './Button/Button'
-export { default as ErrorBoundary } from './ErrorBoundary/ErrorBoundary'
-export { default as LoadingSpinner } from './LoadingSpinner/LoadingSpinner'
-export { default as InputSelect } from './InputSelect/InputSelect'
-export { default as InputField } from './InputField/InputField'
-export { default as Sidebar } from './Sidebar/Sidebar'
+export { default as Button } from "./Button/Button";
+export { default as ErrorBoundary } from "./ErrorBoundary/ErrorBoundary";
+export { default as LoadingSpinner } from "./LoadingSpinner/LoadingSpinner";
+export { default as InputSelect } from "./InputSelect/InputSelect";
+export { default as InputField } from "./InputField/InputField";
+export { default as Sidebar } from "./Sidebar/Sidebar";
